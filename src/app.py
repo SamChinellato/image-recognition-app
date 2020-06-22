a6428be8763a3bb9d36c052e9a7ceca3bd9e7a0e
Samuele Chinellato <samuele.chinellato@bt.com>
2020-06-22 10:15:11 +0100

    Supporting more image file types

--- src/app.py
+++ src/app.py
@@ -2,21 +2,24 @@ import streamlit as st
 from model import predict
 
 # Minimal code for frontend using streamlit
-st.title("Upload An Image to Classify (JPG files only)")
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+st.title("Upload An Image to Classify")
+uploaded_file = st.file_uploader("Choose an image...")
 if uploaded_file is not None:
-    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
-    img = uploaded_file.read()
-    # pass uploaded image to model
     try:
-        result = predict(img)
-        # Result output
-        st.write('%s (%.2f%%)' % (result[0], result[1] * 100))
+        st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
+        st.write("Classifying...")
+        img = uploaded_file.read()
+        # pass uploaded image to model
+        try:
+            result = predict(img)
+            # Result output
+            st.write('%s (%.2f%%)' % (result[0], result[1] * 100))
+        except Exception as e:
+            st.write("Something went wrong, unable to classify image")
+            st.write("For debug: {}".format(e))
     except Exception as e:
-        st.write("Something went wrong, unable to classify image")
-        st.write("For debug: {}".format(e))
+        st.write("Couldn't read that file. Please try another format")
 
 hide_streamlit_style = """
             <style>
